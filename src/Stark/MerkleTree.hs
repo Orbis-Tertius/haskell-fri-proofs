4e912b0d0effc12156c42f9494e19bb5237f2583
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-06-02 09:18:37 -0400

    open_

--- src/Stark/MerkleTree.hs
+++ src/Stark/MerkleTree.hs
@@ -2,6 +2,7 @@ module Stark.MerkleTree
   ( dataToLeaf
   , commit
   , commit_
+  , open_
   ) where
 
 
@@ -10,8 +11,10 @@ import qualified Crypto.Hash.BLAKE2.BLAKE2b as BLAKE2b
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BSL
 
+import qualified Stark.BinaryTree as Tree
+import Stark.Types.AuthPath (AuthPath (AuthPath))
 import Stark.Types.BinaryTree (BinaryTree (IsLeaf, IsNode))
-import Stark.Types.Commitment (Commitment (Commitment))
+import Stark.Types.Commitment (Commitment (Commitment, unCommitment))
 import Stark.Types.Leaf (Leaf (Leaf))
 import Stark.Types.MerkleHash (MerkleHash (MerkleHash))
 
@@ -35,3 +38,15 @@ commit = commit_ . fmap hashData
 commit_ :: BinaryTree MerkleHash -> Commitment
 commit_ (IsLeaf x) = Commitment x
 commit_ (IsNode x y) = Commitment (hashData (commit x, commit y))
+
+
+open_ :: Integer -> BinaryTree MerkleHash -> AuthPath
+open_ 0 (IsNode (IsLeaf _) (IsLeaf x)) = AuthPath [x]
+open_ 1 (IsNode (IsLeaf x) (IsLeaf _)) = AuthPath [x]
+open_ i t@(IsNode x y) =
+  let n = Tree.size t
+      m = n `quot` 2
+  in if i < m
+     then (open_ i x) <> AuthPath [unCommitment $ commit_ y]
+     else (open_ (i-m) x) <> AuthPath [unCommitment $ commit_ y]
+open_ _ _ = error "open_ pattern match failure"
--- src/Stark/Types/AuthPath.hs
+++ src/Stark/Types/AuthPath.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+
 module Stark.Types.AuthPath ( AuthPath (AuthPath, unAuthPath) ) where
 
 
-import Stark.Types.BinaryTree (BinaryTree)
 import Stark.Types.MerkleHash (MerkleHash)
 
 
-newtype AuthPath = AuthPath { unAuthPath :: BinaryTree MerkleHash }
+newtype AuthPath = AuthPath { unAuthPath :: [MerkleHash] }
+  deriving (Semigroup, Monoid)
