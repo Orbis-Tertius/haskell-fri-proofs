7469c2761e87e57e990af719592bc75bf0980ac4
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-06-02 09:47:33 -0400

    finite field order and generator

---
+++ src/Stark/FiniteField.hs
@@ -0,0 +1,15 @@
+module Stark.FiniteField
+  ( order
+  , generator
+  ) where
+
+
+import Stark.Types.Scalar (Scalar)
+
+
+order :: Integer
+order = 270497897142230380135924736767050121217
+
+
+generator :: Scalar
+generator = 85408008396924667383611388730472331217
--- src/Stark/Types/Scalar.hs
+++ src/Stark/Types/Scalar.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 
 module Stark.Types.Scalar ( Scalar (Scalar, unScalar) ) where
@@ -8,3 +9,4 @@ import Math.FiniteField.PrimeField.Generic (Fp)
 
 
 newtype Scalar = Scalar { unScalar :: Fp 270497897142230380135924736767050121217 }
+  deriving Num
