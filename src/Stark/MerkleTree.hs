796a1853849e8b29dbb9ff7ec51108dd538c4e6b
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-06-09 14:14:30 -0400

    pull hash out into its own module

--- src/Stark/MerkleTree.hs
+++ src/Stark/MerkleTree.hs
@@ -10,20 +10,15 @@ module Stark.MerkleTree
 
 
 import Codec.Serialise (Serialise, serialise)
-import qualified Crypto.Hash.BLAKE2.BLAKE2b as BLAKE2b
-import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BSL
 
 import qualified Stark.BinaryTree as Tree
+import Stark.Hash (hash)
 import Stark.Types.AuthPath (AuthPath (AuthPath))
 import Stark.Types.BinaryTree (BinaryTree (IsLeaf, IsNode))
 import Stark.Types.Commitment (Commitment (Commitment, unCommitment))
 import Stark.Types.Leaf (Leaf (Leaf, unLeaf))
 import Stark.Types.MerkleHash (MerkleHash (MerkleHash))
-
-
-hash :: ByteString -> ByteString
-hash = BLAKE2b.hash 64 mempty
 
 
 hashData :: Serialise a => a -> MerkleHash
