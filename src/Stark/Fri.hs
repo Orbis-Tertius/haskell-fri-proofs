30702927432a938610a9ce3900073dbb4fda6a63
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-06-09 14:07:50 -0400

    wip fri

--- src/Stark/Fri.hs
+++ src/Stark/Fri.hs
@@ -1,14 +1,38 @@
 module Stark.Fri
   ( numRounds
+  , evalDomain
+  , sampleIndex
+  , sampleIndices
   ) where
 
 
-import Stark.Fri.Types (DomainLength, ExpansionFactor, NumColinearityTests)
+import Data.Bits (shift, xor)
+import Data.ByteString (ByteString, unpack)
+
+import Stark.Fri.Types (DomainLength (..), ExpansionFactor (..), NumColinearityTests (..), Offset (..), Omega (..), RandomSeed (..), ListSize (..), ReducedListSize (..), Index (..), SampleSize (..))
+import Stark.Types.Scalar (Scalar)
 
 
 numRounds :: DomainLength -> ExpansionFactor -> NumColinearityTests -> Int
-numRounds = todo
+numRounds (DomainLength d) (ExpansionFactor e) (NumColinearityTests n) =
+  if fromIntegral d > e && 4 * n < d
+  then 1 + numRounds
+           (DomainLength (d `div` 2))
+           (ExpansionFactor e)
+           (NumColinearityTests n)
+  else 0
 
 
-todo :: a
-todo = todo
+evalDomain :: Offset -> Omega -> DomainLength -> [Scalar]
+evalDomain (Offset o) (Omega m) (DomainLength d)
+  = [o * (m ^ i) | i <- [0..d-1]]
+
+
+sampleIndex :: ByteString -> Index -> Index
+sampleIndex bs len =
+  foldl (\acc b -> (acc `shift` 8) `xor` fromIntegral b) 0 (unpack bs)
+  `mod` len
+
+
+sampleIndices :: RandomSeed -> ListSize -> ReducedListSize -> SampleSize -> [Index]
+sampleIndices = undefined
--- src/Stark/Fri/Types.hs
+++ src/Stark/Fri/Types.hs
@@ -1,11 +1,22 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+
 module Stark.Fri.Types
   ( Offset (Offset, unOffset)
   , Omega (Omega, unOmega)
   , DomainLength (DomainLength, unDomainLength)
   , ExpansionFactor (ExpansionFactor, unExpansionFactor)
   , NumColinearityTests (NumColinearityTests, unNumColinearityTests)
+  , ListSize (ListSize, unListSize)
+  , RandomSeed (RandomSeed, unRandomSeed)
+  , ReducedListSize (ReducedListSize, unReducedListSize)
+  , SampleSize (SampleSize, unSampleSize)
+  , Index (Index, unIndex)
   ) where
 
+
+import Data.Bits (Bits)
+import Data.ByteString (ByteString)
 
 import Stark.Types.Scalar (Scalar)
 
@@ -23,3 +34,19 @@ newtype ExpansionFactor = ExpansionFactor { unExpansionFactor :: Rational }
 
 
 newtype NumColinearityTests = NumColinearityTests { unNumColinearityTests :: Int }
+
+
+newtype ListSize = ListSize { unListSize :: Int }
+
+
+newtype RandomSeed = RandomSeed { unRandomSeed :: ByteString }
+
+
+newtype ReducedListSize = ReducedListSize { unReducedListSize :: Int }
+
+
+newtype SampleSize = SampleSize { unSampleSize :: Int }
+
+
+newtype Index = Index { unIndex :: Int }
+  deriving (Eq, Ord, Num, Enum, Real, Integral, Bits)
