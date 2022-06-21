ce9c1e8072af6bb0db682d4d6ca9b3525feeee0b
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-06-21 10:13:18 -0400

    stub generators

---
+++ test/Spec/Gen.hs
@@ -0,0 +1,20 @@
+module Spec.Gen
+  ( genFriConfiguration
+  , 
+  ) where
+
+
+genFriConfiguration :: Gen FriConfiguration
+genFriConfiguration = todo
+
+
+genProofStream :: FriConfiguration -> Gen ProofStream
+genProofStream = todo
+
+
+genCodeword :: FriConfiguration -> Gen Codeword
+genCodeword = todo
+
+
+todo :: a
+todo = todo
