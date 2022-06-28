1ff43552650f374f70ba2ae0705d87d407400b48
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-06-27 21:29:13 -0400

    fix test case

--- test/Spec/Stark/MerkleTreeSpec.hs
+++ test/Spec/Stark/MerkleTreeSpec.hs
@@ -3,7 +3,7 @@ module Spec.Stark.MerkleTreeSpec ( spec ) where
 
 import Spec.Gen (genBinaryTree, genScalar)
 import Spec.Prelude
-import Stark.MerkleTree (dataToLeaf, commit, open, verify)
+import Stark.MerkleTree (commit, open, verify)
 import Stark.Prelude (uncurry4)
 import Stark.Types.Index (Index (..))
 
@@ -15,7 +15,7 @@ spec = describe "MerkleTree" $ do
       forAll (Index <$> choose (0, s-1)) $ \i ->
         let c = commit t
             p = open i t
-            l = dataToLeaf (xs !! unIndex i)
-        in (c, i, p, l) `shouldSatisfy` uncurry4 verify
+            x = xs !! unIndex i
+        in (c, i, p, x) `shouldSatisfy` uncurry4 verify
 
   -- it "rejects random inputs" $ return ()
