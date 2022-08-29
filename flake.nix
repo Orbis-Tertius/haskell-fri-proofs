9e86441e7782df1409663f8d7a124554766df4b9
Daniel Firth <dan.firth@homotopic.tech>
2022-08-29 13:37:52 +0100

    Match build plan from prover

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     # Nixpkgs set to specific URL for haskellNix
     nixpkgs.url = "github:NixOS/nixpkgs/baaf9459d6105c243239289e1e82e3cdd5ac4809";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    nixpkgs-2205.url = "github:NixOS/nixpkgs/22.05";
 
     #CI integration
     flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
@@ -31,10 +32,12 @@
 
     lint-utils = {
       url = "git+https://gitlab.homotopic.tech/nix/lint-utils";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs-2205";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, lint-utils, sydtest-src, validity-src, haskellNix, flake-compat, flake-compat-ci }:
+  outputs = { self, nixpkgs, nixpkgs-2205, flake-utils, lint-utils, sydtest-src, validity-src, haskellNix, flake-compat, flake-compat-ci }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         deferPluginErrors = true;
@@ -46,21 +49,24 @@
             fri-proofs =
               final.haskell-nix.project' {
                 src = ./.;
-                compiler-nix-name = "ghc8107";
-                projectFileName = "cabal.project";
+                compiler-nix-name = "ghc922";
+                projectFileName = "stack.yaml";
                 modules = [{
                   packages = { };
                 }];
-                shell.tools = {
-                  cabal = { };
-                  ghcid = { };
-                  hlint = { };
-                  haskell-language-server = { };
-                  stylish-haskell = { };
-                  sydtest-discover = { };
-                };
                 shell.buildInputs = with pkgs; [
-                  nixpkgs-fmt
+                  cabal-install
+                  ghcid
+                  # tools below are used by lint-utils. Since lint-utils follows
+                  # nixpkgs-2205, it only makes sense that tools available on
+                  # the shell are using binaries from the same pkgs
+                  # (22.05). This is a consequence of haskellNix not updating
+                  # its nixpkgs-unstable.
+                  pkgs-2205.haskell-language-server
+                  pkgs-2205.nixpkgs-fmt
+                  pkgs-2205.stylish-haskell
+                  pkgs-2205.hlint
+                  pkgs-2205.haskellPackages.cabal-fmt
                 ];
                 shell.shellHook =
                   ''
@@ -76,6 +82,7 @@
           })
         ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+       pkgs-2205 = import nixpkgs-2205 { inherit system overlays; };
         flake = pkgs.fri-proofs.flake { };
       in
       flake // {
