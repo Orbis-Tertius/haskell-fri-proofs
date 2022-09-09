894138e73ed5e1186a018e4660f4ce668bd2a0d9
Daniel Firth <dan.firth@homotopic.tech>
2022-09-09 11:53:03 +0100

    WIP: define Distributive and Representable for Vect n

--- src/Plonk/Types/Vect.hs
+++ src/Plonk/Types/Vect.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Plonk.Types.Vect
   ( Vect (..)
   , toList
@@ -6,6 +7,9 @@ module Plonk.Types.Vect
 
 import           Data.Kind            (Type)
 import           Data.Vinyl.TypeLevel (Nat (S, Z))
+import Data.Distributive (Distributive (distribute))
+import Data.Functor.Rep (Representable (tabulate, index, type Rep), distributeRep)
+import Plonk.Types.Fin (Fin(FZ, FS), finInj)
 
 type Vect :: Nat -> Type -> Type
 data Vect :: Nat -> Type -> Type where
@@ -21,3 +25,17 @@ instance Functor (Vect m) where
 toList :: Vect n a -> [a]
 toList Nil       = []
 toList (x :- xs) = x : toList xs
+
+instance Representable (Vect ('S n)) => Distributive (Vect ('S n)) where
+  distribute :: Functor f => f (Vect ('S n) b) -> Vect ('S n) (f b)
+  distribute = distributeRep
+
+instance Representable (Vect n) => Representable (Vect ('S n)) where
+  type Rep (Vect ('S n)) = Fin ('S n)
+
+  tabulate :: (Fin ('S n) -> a) -> Vect ('S n) a
+  tabulate f = _
+
+  index :: Vect ('S n) a -> Fin ('S n) -> a
+  index (x :- xs) FZ = x
+  index (x :- xs) (FS a) = index @(Vect n) xs (unsafeCoerce @(Fin n) a)
