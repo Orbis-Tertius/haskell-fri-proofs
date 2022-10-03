b1a8b80261f74849283efefd4a158135d2b82388
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-10-02 20:39:43 -0400

    enable all tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,12 +4,17 @@ module Main
 
 import           Spec.Stark.FriSpec        (testFri)
 import           Spec.Stark.MerkleTreeSpec (testMerkleTree)
+import Spec.Stark.ScalarSpec (testScalar)
+import Spec.Stark.UnivariatePolynomialSpec (testUnivariatePolynomial)
 import           Test.Tasty                (TestTree, defaultMain, testGroup)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [ testFri
-                          , testMerkleTree
-                          ]
+tests = testGroup "Tests"
+  [ testFri
+  , testMerkleTree
+  , testScalar
+  , testUnivariatePolynomial
+  ]
