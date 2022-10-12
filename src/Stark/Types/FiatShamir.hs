13886ca628a39fd61f41fb777e9a2c5b9f795f80
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-10-12 10:36:02 -0400

    fiat shamir revisions

--- src/Stark/Types/FiatShamir.hs
+++ src/Stark/Types/FiatShamir.hs
@@ -27,23 +27,89 @@ class Sampleable a where
 
 
 type IOP :: Type -> Type -> (Type -> Type) -> Type -> Type
-data IOP c t m a where
-  AppendToTranscript :: t -> IOP c t m ()
+data IOP c t w m a where
+  AppendToTranscript
+    :: m t -> IOP c t w m ()
+  Reject :: IOP c t m ()
   SampleChallenge :: IOP c t m c
 
-makeSem ''IOP
-
-fiatShamir
+proverFiatShamir
   :: Sampleable c
   => Serialise t
   => Members '[State t] r
+  => Members '[Input w] r
   => Monoid t
-  => Sem (IOP c t ': r) a -> Sem r a
-fiatShamir = interpret $
-  \case
-    AppendToTranscript t -> do
-     put . (<> t) =<< get
-    SampleChallenge -> do
-     x :: q <- get
-     pure (sample (BSL.toStrict (serialise x)))
+  => w -> Sem (IOP c t w ': r) a -> Sem r a
 
+verifierFiatShamir
+  :: Sampleable c
+  => Serialise t
+  => Members '[Input [t]] r
+  => Sem (IOP c [t] ': r) a -> Sem r a
+
+-- 
+-- 
+-- -- Way 1
+-- 
+-- verify :: Statement -> Transcript -> Sem (IOP Challenge Transcript : r) Bool
+-- 
+-- prove :: Statement -> Witness -> Sem (IOP Challenge Transcript : r) Bool
+-- 
+-- fiatShamir (prove stmt witness)
+-- 
+-- fiatShamir (verify stmt proof)
+-- 
+-- r <- fiatShamir (verify stmt (fiatShamir (prove stmt witness)))
+-- r === True
+-- 
+-- -- Way 2
+-- 
+-- prove :: Statement -> Witness -> Sem r Transcript
+-- 
+-- verify :: Statement -> Transcript -> Sem (IOP Challenge Transcript : r) Bool
+-- 
+-- prove' :: Statement -> Witness -> Sem (IOP Challenge Transcript : r) Bool
+-- prove' stmt witness = verify stmt =<< prove stmt witness
+-- 
+-- makeSem ''IOP
+-- 
+-- fiatShamir
+--   :: Sampleable c
+--   => Serialise t
+--   => Members '[State t] r
+--   => Monoid t
+--   => Sem (IOP c t ': r) a -> Sem r a
+-- fiatShamir = interpret $
+--   \case
+--     AppendToTranscript t -> do
+--      put . (<> t) =<< get
+--     SampleChallenge -> do
+--      x :: q <- get
+--      pure (sample (BSL.toStrict (serialise x)))
+-- 
+-- 
+-- -- Way 3
+-- 
+-- newtype IOP c t m a =
+--   IOP [m (RoundFunction c t m a)]
+--   deriving newtype Monoid
+-- 
+-- 
+-- data RoundFunction c t m a =
+--     ProverRound (t -> m a)
+--   | VerifierRound (t -> VerifierResult c)
+-- 
+-- 
+-- data VerifierResult c =
+--     AcceptsFinally
+--   | Rejects
+--   | Challenges c
+-- 
+-- 
+-- -- Way 4
+-- 
+-- data IOP c t m a w =
+--   IOP
+--   { prove :: t -> w -> m t
+--   , verify :: t -> m Bool
+--   }
